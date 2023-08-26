a86e8872f098943ad24b6c4bd2f1bd70d864f649
yukinouz <47273687+yukinouz@users.noreply.github.com>
2023-08-26 20:41:36 +0900

    build後の出力先をassetsに入れる

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,25 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "./", //相対パスでビルドする
+  base: "./", // 相対パスでビルドする
   root: "src", // 開発ディレクトリ設定
   build: {
     outDir: resolve(__dirname, "dist"),
+    emptyOutDir: true,
     rollupOptions: {
-      input: {
-        home: "src/index.html",
-        about: "src/about/index.html",
+      input: ["src/index.html", "src/about/index.html"],
+      output: {
+        entryFileNames: "assets/js/[name][hash].js",
+        chunkFileNames: "assets/js/[name][hash].js",
+        assetFileNames: (assetInfo) => {
+          if (/\.( gif|jpeg|jpg|png|svg|webp| )$/.test(assetInfo.name)) {
+            return "assets/images/[name][hash].[ext]";
+          }
+          if (/\.css$/.test(assetInfo.name)) {
+            return "assets/css/[name][hash].[ext]";
+          }
+          return "assets/[name][hash].[ext]";
+        },
       },
     },
   },
