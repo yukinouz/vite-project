a5d68be309bababbf8c8d78f334459f95fc8c4e4
yukinouz <47273687+yukinouz@users.noreply.github.com>
2023-08-26 19:30:12 +0900

    [add]stylelint

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "src",
+  base: "./", //相対パスでビルドする
+  root: "src", // 開発ディレクトリ設定
   build: {
     outDir: resolve(__dirname, "dist"),
     rollupOptions: {
